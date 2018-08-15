69b2004ca3554f8616db0c0b140763f0e96b05fc
nikolazelic <nzelic@ymail.com>
2018-08-15 17:56:04 +0200

    Nastavak rada

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@
      <center><h1>ACORN SAMPLE</h1></center>
      <button @click='consolelog'>Console Log output</button>
      <button @click='run'>Run</button>
+     <button @click='calculate'>Calculate</button>
      <textarea v-model='code'></textarea>
      <div class='result'>
-      <VueObjectView v-if='see' ref='display' :value="object" />
+       <VueObjectView v-if='see' ref='display' :value="object" />
+      </div>
+      <div class='calculation'>
+        <VueObjectView v-if='see2' :value="calculation" />
       </div>
   </div>
 </template>
@@ -24,6 +28,8 @@ export default {
       code: 'let a = 5 * 7',
       object: {},
       see: true,
+      see2: true,
+      calculation: {},
     }
   },
   methods: {
@@ -37,6 +43,68 @@ export default {
       setTimeout( ()=>{
         this.see = true;
       },100 );
+    },
+    setCalculation(ob){
+      this.calculation = ob;
+      this.see2 = false;
+      setTimeout(()=>{
+        this.see2 = true;
+      }, 100);
+    },
+    calculate(){
+      // this.setCalculation(this.findAllFunctions());
+      // let f = this.pickFuctionByNmae(this.findAllFunctions(), "ispravna");
+
+      // this.setCalculation(this.getFunctionParamsNames(f));
+      this.setCalculation(this.findAllVariablesDeclaredOnPage());
+    },
+    findAllFunctions(){
+      let ast = a.parse(this.code);
+      let result = [];
+      ast.body.forEach(el=>{
+        if(el.type==='FunctionDeclaration')
+          result.push(el);
+      });
+      return result;
+    },
+    pickFuctionByNmae(arrayAstF, name){
+      for(let i=0; i<arrayAstF.length; i++)
+        if(arrayAstF[i].id.name===name)
+          return arrayAstF[i];
+      return null;
+    },
+    getFunctionParamsNames(astF){
+      let r = [];
+      for(let i=0; i<astF.params.length; i++){
+        r.push(astF.params[i].name);
+      }
+      return r;
+    },
+    findAllVariablesDeclaredOnPage(){
+      var r = [];
+      let ast = a.parse(this.code);
+      for(let i=0; i<ast.body.length; i++){
+        let el = ast.body[i];
+        if(el.type==='VariableDeclaration'){
+          console.log('declaration');
+          var dec = this.getVariblesFromDeclaration(el.declarations);
+          this.appendOnArray(r, dec);
+        }
+      }
+      return r;
+    },
+    getVariblesFromDeclaration(declaration){
+      var r = [];
+      for(let i=0; i<declaration.length; i++)
+        {
+          console.log('variable');
+          r.push(declaration[i].id.name);
+        }
+      return r;
+    },
+    appendOnArray(array, secound){
+      for(let i=0; i<secound; i++)
+        array.push(secound[i]);
     }
   },
   created(){
@@ -55,5 +123,13 @@ textarea{
 .result{
   margin-top: 100px;
   border-top: 3px solid blue;
+}
+.calculation{
+  border-left: 3px solid blue;
+  position: absolute;
+  height: 400px;
+  width: 900px;
+  right: 10px;
+  top: 100px;
 }
 </style>
